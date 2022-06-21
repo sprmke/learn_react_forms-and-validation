68fbfc42915b60346e5fbeb9b525238fd944e58e
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-06-21 18:34:33 +0800

    204. react to lost focus

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -16,6 +16,15 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
@@ -51,6 +60,7 @@ const SimpleInput = (props) => {
           id='name'
           value={enteredName}
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
         />
         {nameInputIsInvalid && (
           <p className='error-text'>Name must not be empty.</p>
